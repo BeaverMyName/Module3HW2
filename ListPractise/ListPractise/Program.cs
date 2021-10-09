559fd648bc600969f3edc8e903f0e6e96c06d244
Igor Bobro <igor.bobro.lul@gmail.com>
2021-10-09 17:59:23 +0300

    task completed

--- ListPractise/ListPractise/Program.cs
+++ ListPractise/ListPractise/Program.cs
@@ -6,6 +6,8 @@ namespace ListPractise
     {
         public static void Main(string[] args)
         {
+            var starter = new Starter();
+            starter.StartApplication();
         }
     }
 }
